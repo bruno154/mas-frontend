e82e4c20d023543bbf92ed98225df05cbd8e8acc
bruno154 <brunovinicius154@gmail.com>
2021-05-16 12:29:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div className="App">
+  <h1>Frontend My Activies Space </h1>
+  </div>
+  );
+  }
+
+  export default App;
